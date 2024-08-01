3437cc6597de479df6a88f8f6e2fb97c6f6e5940
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-08-02 00:30:58 +0530

    Setup React Navigation for Authentication Module

--- App.tsx
+++ App.tsx
@@ -1,20 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import AuthNavigation from "./navigation/AuthNavigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AuthNavigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ navigation/AuthNavigation.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Authenticate from "../screens/authentication/Authenticate";
+import SignUp from "../screens/authentication/SignUp";
+import SignIn from "../screens/authentication/SignIn";
+import ChangePassword from "../screens/authentication/ChangePassword";
+import ForgetPassword from "../screens/authentication/ForgetPassword";
+import EmailVerification from "../screens/authentication/EmailVerification";
+import TwoFactorAuthentication from "../screens/authentication/TwoFactorAuthentication";
+const Stack = createNativeStackNavigator();
+
+const AuthNavigation: React.FC = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="authenticate" component={Authenticate} />
+      <Stack.Screen name="signup" component={SignUp} />
+      <Stack.Screen name="signin" component={SignIn} />
+      <Stack.Screen name="changepassword" component={ChangePassword} />
+      <Stack.Screen name="forgetpassword" component={ForgetPassword} />
+      <Stack.Screen name="emailverification" component={EmailVerification} />
+      <Stack.Screen
+        name="twofactorauthentication"
+        component={TwoFactorAuthentication}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthNavigation;
